8c81413fb9f406843b6a8557deeecd7b38daa648
PraveenJJ <praveen_jj123@yahoo.com>
2024-04-24 12:21:23 +0530

    updated navbar to support scroll feature

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -1,24 +1,50 @@
-function openMenu() {
-    var navbarLinks = document.getElementById("navbarLinks");
-    var hamburgerIcon = document.getElementById("hamburgerIcon");
-    var closeIcon = document.getElementById("closeIcon");
-
-    navbarLinks.classList.toggle("show");
-    hamburgerIcon.classList.toggle("hide");
-    closeIcon.classList.toggle("show");
-}
-
-function closeMenu() {
-    var navbarLinks = document.getElementById("navbarLinks");
-    var hamburgerIcon = document.getElementById("hamburgerIcon");
-    var closeIcon = document.getElementById("closeIcon");
-
-    navbarLinks.classList.toggle("hide");
-    hamburgerIcon.classList.toggle("show");
-    closeIcon.classList.toggle("hide");
-}
-
 function toggle() {
     var navbarContainer = document.querySelector(".navbar-container");
     navbarContainer.classList.toggle("fullscreen");
 }
+
+// best to use
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//     var currentScrollPos = window.pageYOffset;
+//     if (prevScrollpos < currentScrollPos) {
+//         document.getElementById("navbar").style.top = "-91px"; /* Adjust based on navbar height */
+//     } else {
+//         document.getElementById("navbar").style.top = "0";
+//     }
+//     prevScrollpos = currentScrollPos;
+// }
+
+
+var prevScrollpos = window.pageYOffset;
+var navbar = document.getElementById("navbar");
+
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+
+    // Check if the navbar has the class "fullscreen"
+    var isNavbarFullscreen = navbar.classList.contains("fullscreen");
+
+    if (!isNavbarFullscreen) {
+        if (prevScrollpos < currentScrollPos) {
+            navbar.style.top = "-91px"; /* Adjust based on navbar height */
+        } else {
+            navbar.style.top = "0";
+        }
+    }
+    
+    prevScrollpos = currentScrollPos;
+}
+
+// --------------------------------------------------------------
+// don't use this
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function () {
+//     var currentScrollPos = window.pageYOffset;
+//     if (prevScrollpos < currentScrollPos) {
+//         document.getElementById("navbar").classList.add("hidden");
+//     } else {
+//         document.getElementById("navbar").classList.remove("hidden");
+//     }
+//     prevScrollpos = currentScrollPos;
+// }
